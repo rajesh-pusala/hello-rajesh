d19ecd17dedc07d5ef6053ace0d80c5f50e1804f
Raj <rajesh_pusala@infosys.com>
2016-07-07 13:24:53 +0530

    Updated in local
    Updated to test the commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
 
 	public static void main(String args[]){
 		System.out.println("Hello Friend ...!!! Welcome to Java Jenkins Webhooks");
+		System.out.println("Updated in Local ");
 	}
 	
 }	
