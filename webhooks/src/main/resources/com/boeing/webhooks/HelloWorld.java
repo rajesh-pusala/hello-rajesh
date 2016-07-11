76b69d7a67242f73535a4e4157febe56e8db4192
Raj <rajesh_pusala@infosys.com>
2016-07-11 17:03:30 +0530

    commit

--- webhooks/src/main/resources/com/boeing/webhooks/HelloWorld.java
+++ webhooks/src/main/resources/com/boeing/webhooks/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Created Sample Maven Web app to test the jenkins webhooks");
-		System.out.println("Updated to trigger the Jenkins build with Git 2 Push");
+		System.out.println("Updated to trigger the Jenkins build with Git 98 Push");
 	}
 
 }
